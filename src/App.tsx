7ef54d556b3c28bd4e9db2501860b40565f938d4
exit <kalsijatin67@gmail.com>
2024-03-22 18:36:54 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import { theme } from "./common/styles";
-import { TextInputField } from "./common/components/TextInputField";
 
 function App() {
   return (
@@ -10,28 +9,6 @@ function App() {
       <Routes>
         <Route path="/" />
       </Routes>
-      <TextInputField
-        placeholder="ex: janedoe@gmail.com"
-        type="email"
-      ></TextInputField>
-      <br></br>
-      <br></br>
-      <TextInputField
-        placeholder="Enter your password..."
-        type="password"
-      ></TextInputField>
-      <br></br>
-      <br></br>
-      <TextInputField
-        placeholder="ex: janedoe@gmail.com"
-        type="email"
-      ></TextInputField>
-      <br></br>
-      <br></br>
-      <TextInputField
-        placeholder="Enter your password..."
-        type="password"
-      ></TextInputField>
     </ThemeProvider>
   );
 }
