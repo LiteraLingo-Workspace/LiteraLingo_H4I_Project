aea853eb09e44186bf5b680547d7cf428961bfb1
exit <kalsijatin67@gmail.com>
2024-05-08 23:48:55 -0400

    finalized

--- frontend/src/pages/LandingPage/Modal.tsx
+++ frontend/src/pages/LandingPage/Modal.tsx
@@ -5,76 +5,83 @@ import {
   StyledButton,
 } from "../../common/components/index";
 import { theme } from "../../common/styles";
+import { useNavigate } from "react-router-dom";
 
-export const Modal = () => (
-  <StyledDiv
-    display="flex"
-    flexDirection="column"
-    alignItems="center"
-    justifyContent="center"
-    borderTopLeftRadius={64}
-    borderTopRightRadius={64}
-    width="100%"
-    height="39%"
-    bg="white"
-  >
-    <StyledButton
-      bg={theme.colors.coolAccentBg}
-      borderRadius={32}
-      color="white"
-      width="300px"
-      height="67px"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      border="none"
-    >
-      <StyledText
-        color="white"
-        fontSize="32px"
-        lineHeight="51px"
-        fontWeight="500"
-      >
-        {t("landingPage.signIn")}
-      </StyledText>
-    </StyledButton>
+export const Modal = () => {
+  const navigate = useNavigate();
+
+  return (
     <StyledDiv
-      height="1px"
-      width="300px"
-      bg={theme.colors.faintGrey}
-      marginTop="20px"
-      marginBottom="10px"
-    />
-    <StyledText
-      color={theme.colors.secondary}
-      fontSize="20px"
-      lineHeight="30px"
-      fontWeight="400"
-      marginBottom="8px"
-      textAlign="center"
-      fontFamily="Poppins"
-    >
-      {t("landingPage.new")}
-    </StyledText>
-    <StyledButton
-      background="linear-gradient(180deg, #F37576 0%, #F1B950 160%)"
-      borderRadius={32}
-      color="white"
-      width="300px"
-      height="67px"
       display="flex"
-      justifyContent="center"
+      flexDirection="column"
       alignItems="center"
-      border="none"
+      justifyContent="center"
+      borderTopLeftRadius={64}
+      borderTopRightRadius={64}
+      width="100%"
+      height="39%"
+      bg="white"
     >
-      <StyledText
+      <StyledButton
+        bg={theme.colors.coolAccentBg}
+        borderRadius={32}
         color="white"
-        fontSize="32px"
-        lineHeight="51px"
-        fontWeight="500"
+        width="300px"
+        height="67px"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        border="none"
+        onClick={() => navigate("/login")}
       >
-        {t("landingPage.createAccount")}
+        <StyledText
+          color="white"
+          fontSize="32px"
+          lineHeight="51px"
+          fontWeight="500"
+        >
+          {t("landingPage.signIn")}
+        </StyledText>
+      </StyledButton>
+      <StyledDiv
+        height="1px"
+        width="300px"
+        bg={theme.colors.faintGrey}
+        marginTop="20px"
+        marginBottom="10px"
+      />
+      <StyledText
+        color={theme.colors.secondary}
+        fontSize="20px"
+        lineHeight="30px"
+        fontWeight="400"
+        marginBottom="8px"
+        textAlign="center"
+        fontFamily="Poppins"
+      >
+        {t("landingPage.new")}
       </StyledText>
-    </StyledButton>
-  </StyledDiv>
-);
+      <StyledButton
+        background="linear-gradient(180deg, #F37576 0%, #F1B950 160%)"
+        borderRadius={32}
+        color="white"
+        width="300px"
+        height="67px"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        border="none"
+        onClick={() => navigate("/signup")}
+      >
+        <StyledText
+          color="white"
+          fontSize="32px"
+          lineHeight="51px"
+          fontWeight="500"
+        >
+          {t("landingPage.createAccount")}
+        </StyledText>
+      </StyledButton>
+    </StyledDiv>
+  );
+};
