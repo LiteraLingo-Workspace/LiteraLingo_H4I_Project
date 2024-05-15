1bc68fd5be35463120c82de62972d77a3b452735
exit <kalsijatin67@gmail.com>
2024-05-14 21:48:16 -0400

    homepage welcome

---
+++ frontend/src/pages/HomePage/Welcome.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { avatar } from "../../../images";
+import { theme } from "../../common/styles";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  position: relative;
+  top: 68px;
+`;
+
+const NameContainer = styled.div`
+  color: ${theme.colors.primary};
+  display: flex;
+  flex-direction: column;
+  width: 98px;
+  height: 74px;
+  text-align: right;
+`;
+
+export const Welcome: React.FC = () => {
+  return (
+    <Container>
+      <img
+        style={{ height: "80px", width: "75.35px", borderRadius: "100px" }}
+        src={avatar}
+      />
+      <NameContainer>
+        <p
+          style={{
+            fontFamily: "'Poppins', sans-serif",
+            fontWeight: "500",
+            fontSize: "20px",
+            lineHeight: "30px",
+          }}
+        >
+          Welcome
+        </p>
+        <p
+          style={{
+            fontFamily: "'Baloo 2', sans-serif",
+            fontWeight: "500",
+            fontSize: "32px",
+            lineHeight: "51.26px",
+          }}
+        >
+          Anna
+        </p>
+      </NameContainer>
+    </Container>
+  );
+};
--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,3 +1,39 @@
+import styled from "styled-components";
+import { Background } from "./Background";
+import { Welcome } from "./Welcome";
+
+const Container = styled.div`
+  background-color: transparent;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SubContainer = styled.div`
+  position: absolute;
+  border: 1px solid black;
+  width: 344px;
+  height: 100%;
+  z-index: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const HomePage: React.FC = () => {
-  return <p>foppy was here</p>;
+  return (
+    <Container>
+      <Background />
+      <SubContainer>
+        <Welcome />
+      </SubContainer>
+    </Container>
+  );
 };
