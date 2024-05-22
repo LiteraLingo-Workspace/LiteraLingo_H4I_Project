085a0f35a4315e85c2fbb2b0cdb98588df6f5d2f
exit <kalsijatin67@gmail.com>
2024-05-22 19:45:38 -0400

    bg 100% size

--- frontend/src/pages/HomePage/Background.tsx
+++ frontend/src/pages/HomePage/Background.tsx
@@ -1,8 +1,8 @@
 export const Background: React.FC = () => {
   return (
     <svg
-      width="393"
-      height="590"
+      width="100%"
+      height="100%"
       viewBox="0 0 393 590"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
