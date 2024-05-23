1866993b8c47ccfdd6f1101695188e60b79c1799
karangandhi272 <65358208+karangandhi272@users.noreply.github.com>
2024-05-22 23:31:07 -0400

    inital commit of savedpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { theme } from "./common/styles";
 import GlobalStyle from "./common/styles/GlobalStyle";
 import { LandingPage, UserAuthPage } from "./pages/index";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {SavedPage} from "./pages/SavedPage/SavedPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/saved" element={<SavedPage />} />
           <Route path="/login" element={<UserAuthPage type="login" />} />
           <Route path="/signup" element={<UserAuthPage type="signup" />} />
         </Routes>
