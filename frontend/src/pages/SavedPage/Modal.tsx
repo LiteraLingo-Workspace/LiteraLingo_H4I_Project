453675beab8fa1bd128ee67e297c775c02949d71
exit <kalsijatin67@gmail.com>
2024-05-24 21:19:02 -0400

    minor changes

--- frontend/src/pages/SavedPage/Modal.tsx
+++ frontend/src/pages/SavedPage/Modal.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
-import { VscKebabVertical } from "react-icons/vsc";
-import { FaStar, FaRegStar } from "react-icons/fa6";
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { theme } from "../../common/styles/index";
-
+import { Item } from "./Item";
 
 const SavedPage = styled.div`
   display: flex;
@@ -11,21 +9,17 @@ const SavedPage = styled.div`
   height: 100vh;
   z-index: 1;
   position: relative;
-  overflow: hidden
-
+  overflow: hidden;
 `;
 
 const Header = styled.div`
   padding-top: 68px;
   padding-left: 30px;
-
-  position:fixed;
-
+  position: fixed;
 `;
 
 const HeaderTitle = styled.h1`
   margin: 0;
-
   font-family: "Poppins", sans-serif;
   color: ${theme.colors.primary};
   font-weight: 700;
@@ -35,92 +29,44 @@ const HeaderTitle = styled.h1`
 const SavedItems = styled.div`
   flex: 1;
   margin: 16px;
-  top:94px;
+  top: 94px;
   overflow-y: auto;
   -webkit-overflow-scrolling: touch;
   position: relative;
-
-
 `;
 
-const Top = styled.div`
-  display: flex;
-  width: 100%;
-  height: 32px;
-  justify-content: space-between;
-`;
-
-const SavedItem = styled.div`
-  background-color: ${theme.colors.faintBlue};
-  margin-bottom: 10px;
-  margin-left: 10px;
-  margin-right: 10px;
-  padding: 8px;
-  border-radius: 12px;
-  display: flex;
-  justify-content: space-evenly;
-  flex-direction: column;
-`;
-
-const ItemType = styled.div`
-  color: ${theme.colors.secondary}
-  font-size: 16px;
-  font-family: "Poppins", sans-serif;
-`;
-
-const ItemText = styled.div`
-  flex: 1;
-  font-size: 20px;
-  font-weight: 500;
-  color: ${theme.colors.primary};
-  font-family: "Poppins", sans-serif;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  margin-right: 8px;
-`;
-
-const ItemActions = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-//might have to adjust the height to account of the navbar 
-const Space = styled.div `
+//might have to adjust the height to account of the navbar
+const Space = styled.div`
   width: 100%;
   height: 150px;
-
 `;
-
-
-
 
 export const Modal: React.FC = () => {
   const items = [
-    { id: 1, type: 'Metonymy', text: 'Use soft words and hard argument' },
-    { id: 2, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 3, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 4, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 5, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 6, type: 'Proverb', text: 'Use soft words and hard argument' },
-    { id: 1, type: 'Metonymy', text: 'Use soft words and hard argument' },
-    { id: 2, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 3, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 4, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 5, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 6, type: 'Proverb', text: 'Use soft words and hard argument' },
-    { id: 1, type: 'Metonymy', text: 'Use soft words and hard argument' },
-    { id: 2, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 3, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 4, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 5, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 6, type: 'Proverb', text: 'Use soft words and hard argument' },
-    { id: 1, type: 'Metonymy', text: 'Use soft words and hard argument' },
-    { id: 2, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 3, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 4, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 5, type: 'Simile', text: 'This winter is as cold as death' },
-    { id: 6, type: 'Proverb', text: 'Use soft words and hard argument' },
+    { id: 1, type: "Metonymy", text: "Use soft words and hard argument" },
+    { id: 2, type: "Simile", text: "This winter is as cold as death" },
+    { id: 3, type: "Simile", text: "This winter is as cold as death" },
+    { id: 4, type: "Simile", text: "This winter is as cold as death" },
+    { id: 5, type: "Simile", text: "This winter is as cold as death" },
+    { id: 6, type: "Proverb", text: "Use soft words and hard argument" },
+    { id: 1, type: "Metonymy", text: "Use soft words and hard argument" },
+    { id: 2, type: "Simile", text: "This winter is as cold as death" },
+    { id: 3, type: "Simile", text: "This winter is as cold as death" },
+    { id: 4, type: "Simile", text: "This winter is as cold as death" },
+    { id: 5, type: "Simile", text: "This winter is as cold as death" },
+    { id: 6, type: "Proverb", text: "Use soft words and hard argument" },
+    { id: 1, type: "Metonymy", text: "Use soft words and hard argument" },
+    { id: 2, type: "Simile", text: "This winter is as cold as death" },
+    { id: 3, type: "Simile", text: "This winter is as cold as death" },
+    { id: 4, type: "Simile", text: "This winter is as cold as death" },
+    { id: 5, type: "Simile", text: "This winter is as cold as death" },
+    { id: 6, type: "Proverb", text: "Use soft words and hard argument" },
+    { id: 1, type: "Metonymy", text: "Use soft words and hard argument" },
+    { id: 2, type: "Simile", text: "This winter is as cold as death" },
+    { id: 3, type: "Simile", text: "This winter is as cold as death" },
+    { id: 4, type: "Simile", text: "This winter is as cold as death" },
+    { id: 5, type: "Simile", text: "This winter is as cold as death" },
+    { id: 6, type: "Proverb", text: "Use soft words and hard argument" },
   ];
 
   return (
@@ -130,45 +76,11 @@ export const Modal: React.FC = () => {
       </Header>
       <SavedItems>
         {items.map((item, index) => (
-          <Item key={index} id = {item.id} type = {item.type} text = {item.text} />
-         
+          <Item key={index} id={item.id} type={item.type} text={item.text} />
         ))}
-        <Space>
-
-        </Space>
+        <Space></Space>
       </SavedItems>
-
     </SavedPage>
-  );
-};
-
-interface itemProps  {
-  id: number; 
-  type: string; 
-  text: string; 
-}
-const Item: React.FC<itemProps> = ({ id, type, text }) => {
-  const [saved, setSaved] = useState(false);
-
-  return (
-    <SavedItem key={id}>
-
-            <Top>
-
-                <ItemType>{type}</ItemType>
-
-
-                <ItemActions>
-                {saved ? (<FaStar  style={{ color: '#F1B950'}} onClick={() => setSaved(!saved)}/>) : (<FaRegStar onClick={() => setSaved(!saved)}/>)}
-      
-                <VscKebabVertical />
-                </ItemActions>
-
-            </Top>
-
-
-            <ItemText className="item-text">{text}</ItemText>
-          </SavedItem>
   );
 };
 
--- frontend/src/pages/SavedPage/index.tsx
+++ frontend/src/pages/SavedPage/index.tsx
@@ -1,12 +1,10 @@
-
 import { Background } from "./Background";
 import { Modal } from "./Modal";
-
 
 export const SavedPage: React.FC = () => {
   return (
     <div>
-      <Background/>
+      <Background />
       <Modal />
     </div>
   );
