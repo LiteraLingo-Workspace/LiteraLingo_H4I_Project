2bbbc82723ec7116383b6c43f074e9816e29c158
exit <kalsijatin67@gmail.com>
2024-05-25 13:41:46 -0400

    quiz page done

--- frontend/src/pages/QuizPage/MultipleChoice.tsx
+++ frontend/src/pages/QuizPage/MultipleChoice.tsx
@@ -8,6 +8,12 @@ const Container = styled.div`
   width: 344px;
 `;
 
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 25px;
+`;
+
 const ChoicesContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -26,6 +32,7 @@ const ChoiceContainer = styled.div`
   border: 2px solid ${theme.colors.coolDark};
   border-radius: 16px;
   box-shadow: 0px 4px 0px 0px rgba(100, 174, 198, 1);
+  cursor: pointer;
 `;
 
 const ChoiceText = styled.p`
@@ -37,32 +44,51 @@ const ChoiceText = styled.p`
   color: ${theme.colors.primary};
 `;
 
+const Button = styled.button`
+  height: 48px;
+  border-radius: 100px;
+  padding: 8px 12px 8px 12px;
+  background-color: rgba(229, 229, 229, 1);
+  color: rgba(51, 51, 51, 1);
+  font-family: "Baloo 2", sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 32px;
+  border: none;
+  cursor: pointer;
+`;
+
 export const MultipleChoice: React.FC = () => {
   return (
-    <Container>
-      <p
-        style={{
-          fontFamily: "Poppins, sans-serif",
-          fontWeight: 700,
-          fontSize: "22px",
-          lineHeight: "33px",
-          color: `${theme.colors.primary}`,
-          textAlign: "left",
-        }}
-      >
-        What does this mean?
-      </p>
-      <ChoicesContainer>
-        <ChoiceContainer>
-          <ChoiceText>Her promises can be believed.</ChoiceText>
-        </ChoiceContainer>
-        <ChoiceContainer>
-          <ChoiceText>Her promises cannot be trusted.</ChoiceText>
-        </ChoiceContainer>
-        <ChoiceContainer>
-          <ChoiceText>Her words sound challenging to understand.</ChoiceText>
-        </ChoiceContainer>
-      </ChoicesContainer>
-    </Container>
+    <>
+      <Container>
+        <p
+          style={{
+            fontFamily: "Poppins, sans-serif",
+            fontWeight: 700,
+            fontSize: "22px",
+            lineHeight: "33px",
+            color: `${theme.colors.primary}`,
+            textAlign: "left",
+          }}
+        >
+          What does this mean?
+        </p>
+        <ChoicesContainer>
+          <ChoiceContainer>
+            <ChoiceText>Her promises can be believed.</ChoiceText>
+          </ChoiceContainer>
+          <ChoiceContainer>
+            <ChoiceText>Her promises cannot be trusted.</ChoiceText>
+          </ChoiceContainer>
+          <ChoiceContainer>
+            <ChoiceText>Her words sound challenging to understand.</ChoiceText>
+          </ChoiceContainer>
+        </ChoicesContainer>
+      </Container>
+      <ButtonContainer>
+        <Button>Check answer</Button>
+      </ButtonContainer>
+    </>
   );
 };
