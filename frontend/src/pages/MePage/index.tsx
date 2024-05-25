988efb1120ea4d519735b0777b407b35c095d4a2
exit <kalsijatin67@gmail.com>
2024-05-25 00:43:21 -0400

    user identity complete

--- frontend/src/pages/MePage/index.tsx
+++ frontend/src/pages/MePage/index.tsx
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+import { Header } from "../../common/components/Header";
+import { UserInfo } from "./UserInfo";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export const MePage: React.FC = () => {
-  return <p>dummy me page</p>;
+  return (
+    <Container>
+      <Header title="Your Profile" />
+      <UserInfo />
+    </Container>
+  );
 };
