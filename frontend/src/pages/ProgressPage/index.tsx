8a77ba3851df3c380d3166a6d8b320b594dd8eb3
exit <kalsijatin67@gmail.com>
2024-05-25 14:06:52 -0400

    small whitespace changes

--- frontend/src/pages/ProgressPage/index.tsx
+++ frontend/src/pages/ProgressPage/index.tsx
@@ -3,9 +3,8 @@ import { Navbar } from "../../common/components/Navbar";
 export const ProgressPage: React.FC = () => {
   return (
     <div>
-    <p>dummy progress page</p>
-    <Navbar/>
+      <p>dummy progress page</p>
+      <Navbar />
     </div>
-
-  )
+  );
 };
