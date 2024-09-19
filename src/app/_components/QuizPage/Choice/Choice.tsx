a89816d498b7b7302a14e906cd6a222adf11c62e
wasifsomji <wasifsomji@gmail.com>
2024-09-19 19:20:27 -0400

    feature working

---
+++ src/app/_components/QuizPage/Choice/Choice.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Choice.module.css";
+
+interface ChoiceProps {
+  text: string;
+  isCorrect: boolean;
+  selected: boolean;
+  onClick: () => void;
+}
+
+export const Choice: React.FC<ChoiceProps> = ({ text, selected, onClick }) => {
+    return (
+      <div
+        className={selected ? styles.selectedChoiceContainer : styles.choiceContainer}
+        onClick={onClick}
+      >
+        <p className={selected ? styles.selectedChoiceText : styles.choiceText}>{text}</p>
+      </div>
+    );
+  };
--- src/app/_components/QuizPage/MultipleChoice/MultipleChoice.tsx
+++ src/app/_components/QuizPage/MultipleChoice/MultipleChoice.tsx
@@ -1,26 +1,55 @@
+'use client';
+
+import { useState } from "react";
+import { Choice } from "../Choice";
 import styles from "./MultipleChoice.module.css";
 
 export const MultipleChoice: React.FC = () => {
+  const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
+  const [isCorrectChoice, setIsCorrectChoice] = useState<boolean | null>(null);
+
+  const handleChoiceClick = (choice: string, isCorrect: boolean) => {
+    setSelectedChoice(choice);
+    setIsCorrectChoice(isCorrect);
+  }
+
+  const handleCheckAnswer = () => {
+    if (isCorrectChoice) {
+      console.log("Correct!");
+    } else {
+      console.log("Incorrect!");
+    }
+  }
+
   return (
     <>
       <div className={styles.container}>
         <p className={styles.questionLabel}>What does this mean?</p>
         <div className={styles.choicesContainer}>
-          <div className={styles.choiceContainer}>
-            <p className={styles.choiceText}>Her promises can be believed.</p>
-          </div>
-          <div className={styles.choiceContainer}>
-            <p className={styles.choiceText}>Her promises cannot be trusted.</p>
-          </div>
-          <div className={styles.choiceContainer}>
-            <p className={styles.choiceText}>
-              Her words sound challenging to understand.
-            </p>
-          </div>
+
+          <Choice 
+            text="Her promises can be believed." 
+            isCorrect={true}
+            selected={selectedChoice === "Her promises can be believed."}
+            onClick={() => handleChoiceClick("Her promises can be believed.", true)}
+          />
+          <Choice 
+            text="Her promises cannot be trusted." 
+            isCorrect={false}
+            selected={selectedChoice === "Her promises cannot be trusted."}
+            onClick={() => handleChoiceClick("Her promises cannot be trusted.", false)}
+          />
+          <Choice 
+            text="Her words sound challenging to understand." 
+            isCorrect={false}
+            selected={selectedChoice === "Her words sound challenging to understand."}
+            onClick={() => handleChoiceClick("Her words sound challenging to understand.", false)}
+          />
+          
         </div>
       </div>
       <div className={styles.buttonContainer}>
-        <button className={styles.button}>Check answer</button>
+        <button className={styles.button}  onClick={handleCheckAnswer}>Check Answer</button>
       </div>
     </>
   );
