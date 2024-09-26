38b7dcbce4b349259f0d4d9e783d77268b0b258c
wasifsomji <wasifsomji@gmail.com>
2024-09-26 18:50:20 -0400

    api working

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,11 @@
 import { userRouter } from "~/server/api/routers/user";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { historyEntryRouter } from "./routers/historyEntry";
 
 // primary router for your server -- all routers added in /api/routers should be manually added here
 export const appRouter = createTRPCRouter({
   user: userRouter,
+  historyEntry: historyEntryRouter,
 });
 
 // export type definition of API
