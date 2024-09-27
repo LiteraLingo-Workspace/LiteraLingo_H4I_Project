cc5d6e72bb3027647c04ca127ffbe14e280727b9
Killua <thomas.nguyen3@mail.mcgill.ca>
2024-09-27 16:48:37 -0400

    historyEntry route create with updateIsFavorite and getHistoryEntryById methods

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,11 @@
 import { userRouter } from "~/server/api/routers/user";
+import { historyEntryRouter } from "~/server/api/routers/historyEntry";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 // primary router for your server -- all routers added in /api/routers should be manually added here
 export const appRouter = createTRPCRouter({
   user: userRouter,
+  historyEntry: historyEntryRouter,
 });
 
 // export type definition of API
