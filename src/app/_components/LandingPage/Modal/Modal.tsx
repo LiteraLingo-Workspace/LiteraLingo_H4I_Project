6ebff39c1d32fff860c08b9112bb36094e23e2c3
exit <kalsijatin67@gmail.com>
2024-09-27 21:04:16 -0400

    basic user auth done

--- src/app/_components/LandingPage/Modal/Modal.tsx
+++ src/app/_components/LandingPage/Modal/Modal.tsx
@@ -1,22 +1,21 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import styles from "./Modal.module.css";
 import { getServerAuthSession } from "~/server/auth";
 
 export const Modal: React.FC = async () => {
   const session = await getServerAuthSession();
-  const router = useRouter();
 
   return (
     <div className={styles.container}>
-      <button
-        className={styles.loginButton}
-        onClick={() =>
-          router.push(session ? "/api/auth/signout" : "/api/auth/signin")
-        }
-      >
-        <p className={styles.buttonText}>{session ? "Sign out" : "Sign in"}</p>
+      <button className={styles.loginButton}>
+        <Link
+          style={{ textDecoration: "none" }}
+          href={session ? "/api/auth/signout" : "/api/auth/signin"}
+        >
+          <p className={styles.buttonText}>
+            {session ? "Sign out" : "Sign in"}
+          </p>
+        </Link>
       </button>
       <div className={styles.break} />
       <p>{session && <span>Logged in as {session.user?.name}</span>}</p>
