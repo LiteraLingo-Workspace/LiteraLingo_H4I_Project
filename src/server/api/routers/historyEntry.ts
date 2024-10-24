a39bcd65c0d2598e7c086938dcbca4bcad16ed1a
wasifsomji <wasifsomji@gmail.com>
2024-10-24 17:43:10 -0400

    history entry endpoint working

--- src/server/api/routers/historyEntry.ts
+++ src/server/api/routers/historyEntry.ts
@@ -1,27 +1,25 @@
 import { z } from "zod";
-import { HistoryEntry } from "@prisma/client";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const historyEntryRouter = createTRPCRouter({
-    // add history entry API
-    create: publicProcedure
+  // add history entry API
+  create: publicProcedure
     .input(
-       z.object({
+      z.object({
         textEntered: z.string().min(1),
         outputText: z.string().min(1),
         isFavorite: z.boolean().optional(),
         historyEntryId: z.number(),
-       }) 
+      }),
     )
     .mutation(async ({ ctx, input }) => {
-        return ctx.db.historyEntry.create({
-            data: {
-                textEntered: input.textEntered,
-                outputText: input.outputText,
-                isFavorite: input.isFavorite,
-                historyEntryId: input.historyEntryId,
-            }
-        })
+      return ctx.db.historyEntry.create({
+        data: {
+          textEntered: input.textEntered,
+          outputText: input.outputText,
+          isFavorite: input.isFavorite,
+          historyEntryId: input.historyEntryId,
+        },
+      });
     }),
 });
