1d28132ec2bd7b5dcf471bf7dfb8a0b770cb580e
siwuagain <01simon.wu@gmail.com>
2024-10-24 19:50:31 -0400

    get user api added

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+    // Endpoint to fetch a User by ID
+    // Example: GET http://localhost:3000/api/trpc/historyEntry.getHistoryEntryById?batch=1&input={"0":{"json": {"id": <account id>}}}
+  getUserById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const user = await ctx.db.user.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+      console.log(user);
+      return user;
+    }),
+});
