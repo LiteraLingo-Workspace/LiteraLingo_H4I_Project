6203c93e34bfc26734bac2dd41c8dcdaf5d678d2
siwuagain <01simon.wu@gmail.com>
2024-10-31 18:53:48 -0400

    adjust comment

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,7 +3,7 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
     // Endpoint to fetch a User by ID
-    // Example: GET http://localhost:3000/api/trpc/historyEntry.getHistoryEntryById?batch=1&input={"0":{"json": {"id": <account id>}}}
+    // Example: GET http://localhost:3000/api/trpc/user.getUserById?batch=1&input={"0":{"json": {"id": "account id"}}}
   getUserById: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
