128c903527383aa568dd1c17713295abbcca313b
mintary <ewang0332@gmail.com>
2024-11-07 19:06:48 -0500

    display correct answer

--- src/app/_components/QuizPage/Choice/Choice.tsx
+++ src/app/_components/QuizPage/Choice/Choice.tsx
@@ -21,7 +21,7 @@ export const Choice: React.FC<ChoiceProps> = ({
   return (
     <div
       className={`${selected ? styles.selectedChoiceContainer : styles.choiceContainer} 
-                  ${(isChecked && selected) && (isCorrect ? styles.correctChoice : styles.incorrectChoice)}`}
+                  ${(isChecked && (selected || isCorrect)) && (isCorrect ? styles.correctChoice : styles.incorrectChoice)}`}
       onClick={disabled ? undefined : onClick}
     >
       <p className={selected ? styles.selectedChoiceText : styles.choiceText}>
