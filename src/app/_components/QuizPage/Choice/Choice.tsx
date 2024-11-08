56799b2639e853fa6da623e05946486cd3bcae3d
mintary <ewang0332@gmail.com>
2024-11-07 19:17:02 -0500

    change colors, display correct answer if incorrect

--- src/app/_components/QuizPage/Choice/Choice.tsx
+++ src/app/_components/QuizPage/Choice/Choice.tsx
@@ -24,7 +24,7 @@ export const Choice: React.FC<ChoiceProps> = ({
                   ${(isChecked && (selected || isCorrect)) && (isCorrect ? styles.correctChoice : styles.incorrectChoice)}`}
       onClick={disabled ? undefined : onClick}
     >
-      <p className={selected ? styles.selectedChoiceText : styles.choiceText}>
+      <p className={(selected && !isChecked) ? styles.selectedChoiceText : styles.choiceText}>
         {text}
       </p>
     </div>
