06ba3dd17c0c3d61ed7cf106dfd870cc6c4967c9
mintary <ewang0332@gmail.com>
2024-11-21 16:11:47 -0500

    add deletion route

--- src/server/api/routers/account.ts
+++ src/server/api/routers/account.ts
@@ -4,7 +4,7 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 export const accountRouter = createTRPCRouter({
   // Endpoint to fetch an Account by Id
   // Example: GET http://localhost:3000/api/trpc/account.getAccountbyId?batch=1&input={"0":{"json": {"id": "account id"}}}
-  getAccountbyId: publicProcedure
+  getAccountById: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
       const user = await ctx.db.account.findUnique({
@@ -16,5 +16,39 @@ export const accountRouter = createTRPCRouter({
         throw new Error("Account not found");
       }
       return user;
+    }),
+
+  // Endpoint to fetch account(s) by user ID
+  // Example: GET http://localhost:3000/api/trpc/account.getAccountbyUserId?batch=1&input={"0":{"json": {"userId": "user id"}}}
+  getAccountByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const user = await ctx.db.account.findMany({
+        where: {
+          userId: input.userId,
+        },
+      });
+      if (!user) {
+        throw new Error("Account not found");
+      }
+      return user;
+    }),
+
+  // Endpoint to delete user by user ID
+  // Example: GET http://localhost:3000/api/trpc/account.deleteAccountbyUserId?batch=1&input={"0":{"json": {"userId": "user id"}}}
+  deleteAccountByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const deletedAccount = await ctx.db.account.deleteMany({
+        where: {
+          userId: input.userId,
+        },
+      });
+
+      if (!deletedAccount) {
+        throw new Error("Account was not deleted properly");
+      }
+
+      return deletedAccount;
     }),
 });
