d2419b9940be8d97abc1867ad6b01cf10d8c77b0
James <jamesbmadden@gmail.com>
2025-03-20 19:09:07 -0400

    add a nicer loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,24 @@
+import { LoadingPage } from './_components/LoadingPage/index';
+import { Poppins, Baloo_2 } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const baloo_2 = Baloo_2({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-baloo-2",
+  weight: ["400", "500", "600", "700", "800"],
+});
+
 export default function Loading() {
-  return <p>Loading...</p>;
+  return (
+    <main className={`${poppins.variable} ${baloo_2.variable}`}>
+      <LoadingPage />
+    </main>
+  );
 }
